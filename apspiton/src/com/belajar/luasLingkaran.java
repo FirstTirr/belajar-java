c8c4476fbbac96768b69a2c0c79cd3b2f91ddd95
FirstTirr <fathiradzans@gmail.com>
2025-02-12 11:58:30 +0700

    yang di upgrade

--- apspiton/src/com/belajar/luasLingkaran.java
+++ apspiton/src/com/belajar/luasLingkaran.java
@@ -1,23 +1,54 @@
 package com.belajar;
+
 import java.util.Scanner;
+
 public class luasLingkaran {
+
     public static void main(String[] args) {
         luasLingkaran lingkaran = new luasLingkaran();
-        lingkaran.isi();
-        lingkaran.hitung();
+        try (Scanner scanner = new Scanner(System.in)) {
+            int pilihan;
+
+            do {
+                System.out.println("1. luas lingkaran");
+                System.out.println("2. keliling lingkaran");
+                System.out.println("3. keluar");
+                System.out.println("pilih menu");
+                pilihan = scanner.nextInt();
+
+                switch (pilihan) {
+                    case 1:
+                        lingkaran.hitungLuasLingkaran(scanner);
+                        break;
+                    case 2:
+                        lingkaran.hitungKelilingLingkaran(scanner);
+                        break;
+                    case 3:
+                        System.out.println("terima kasih");
+                        break;
+                    default:
+                        System.out.println("pilihan tidak ada");
+                        break;
+                }
+                System.out.println();
+            } while (pilihan != 3);
+        }
     }
 
-    int jari;
+    int jari, diameter;
     double phi = 3.14;
-    Scanner scanner = new Scanner(System.in);
 
-    void isi () {
-        System.out.print("Masukkan jari-jari lingkaran: ");
-        jari = scanner.nextInt();
+    void hitungLuasLingkaran(Scanner scanner) {
+        System.out.print("Masukkan jari-jari lingkaran untuk menghitung luas: ");
+        int jariLuas = scanner.nextInt();
+        double luas = phi * jariLuas * jariLuas;
+        System.out.println("Luas lingkaran adalah: " + luas);
     }
 
-    void hitung () {
-        double luas = phi * jari * jari;
-        System.out.println("Luas lingkaran adalah: " + luas);
+    void hitungKelilingLingkaran(Scanner scanner) {
+        System.out.print("Masukkan diameter lingkaran untuk menghitung keliling: ");
+        int diameterKeliling = scanner.nextInt();
+        double keliling = phi * diameterKeliling;
+        System.out.println("Keliling lingkaran adalah: " + keliling);
     }
 }
